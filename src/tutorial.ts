ca71d01f837eca6c74618e68e04d7e6d20ccc9b6
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-12-15 04:43:09 +0545

    Initial

---
+++ src/tutorial.ts
@@ -0,0 +1,13 @@
+console.log('Hello from tutorial.ts file!')
+
+interface someValue{
+    name:String,
+    id:number,
+}
+
+let someObj:someValue={
+    name: 'Harry',
+    id:1,
+}
+
+console.log(someObj)
